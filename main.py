c1e9fab92b8b08a8b4dafdcd07a9aac4d720f7cd
Eric MADEC <eric.madec@ecmorlaix.fr>
2023-09-24 13:06:16 +0200

    MAJ

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
-print("Demat d'an holl !")
-print("Toto est dans la place !")
+print("Demat d'an holl en console !")
+display("Demat d'an holl sur cette page !")
+print("Toto est dans la console !")
+display("Toto est sur cette page !")
